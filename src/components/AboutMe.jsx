fc726e96d3067100a0a2e57ae4efd52299dbbacc
edward <edwardlfish@gmail.com>
2026-05-20 19:11:40 +0300

    Cache API responses for `AboutMe` and `OpeningCrawl` components using localStorage and sessionStorage, respectively, to optimize performance.

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,25 +1,36 @@
-import {baseUrl} from "../utils/constants.js";
+import {baseUrl, periodMonth} from "../utils/constants.js";
 import {useEffect, useState} from "react";
 
 const AboutMe = () => {
-    const [hero, setHero] = useState();
+    const [hero, setHero] = useState(() => {
+        const hero = JSON.parse(localStorage.getItem('hero'));
+        if (hero && Date.now() - hero.timestamp < periodMonth) {
+            return hero.payload;
+        }
+    });
 
     useEffect(() => {
-        fetch(`${baseUrl}/v1/peoples/1`)
-            .then(response => response.json())
-            .then(data => {
-                const info = {
-                    name: data.name,
-                    gender: data.gender,
-                    birth_year: data.birth_year,
-                    height: data.height,
-                    mass: data.mass,
-                    hair_color: data.hair_color,
-                    skin_color: data.skin_color,
-                    eye_color: data.eye_color
-                }
-                setHero(info);
-            })
+        if (!hero) {
+            fetch(`${baseUrl}/v1/peoples/1`)
+                .then(response => response.json())
+                .then(data => {
+                    const info = {
+                        name: data.name,
+                        gender: data.gender,
+                        birth_year: data.birth_year,
+                        height: data.height,
+                        mass: data.mass,
+                        hair_color: data.hair_color,
+                        skin_color: data.skin_color,
+                        eye_color: data.eye_color
+                    }
+                    setHero(info);
+                    localStorage.setItem('hero', JSON.stringify({
+                        payload: info,
+                        timestamp: Date.now()
+                    }));
+                })
+        }
     }, [])
 
 
