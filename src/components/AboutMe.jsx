7c8329d5d715fae3fba31d36c41b5aa1538470ee
edward <edwardlfish@gmail.com>
2026-05-20 18:06:27 +0300

    Fetch and display character data dynamically in `AboutMe` component.

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,9 +1,45 @@
+import {baseUrl} from "../utils/constants.js";
+import {useEffect, useState} from "react";
+
 const AboutMe = () => {
+    const [hero, setHero] = useState();
+
+    useEffect(() => {
+        fetch(`${baseUrl}/v1/peoples/1`)
+            .then(response => response.json())
+            .then(data => {
+                const info = {
+                    name: data.name,
+                    gender: data.gender,
+                    birth_year: data.birth_year,
+                    height: data.height,
+                    mass: data.mass,
+                    hair_color: data.hair_color,
+                    skin_color: data.skin_color,
+                    eye_color: data.eye_color
+                }
+                setHero(info);
+            })
+    }, [])
+
+
     return (
-        <div>
-            About Me
-        </div>
-    )
+        <>
+            {(!!hero) &&
+                <div className='fs-2 lh-lg text-justify ms-5'>
+                    <p><span className='display-3'>name:</span> {hero.name}</p>
+                    <p><span className='display-3'>gender:</span> {hero.gender}</p>
+                    <p><span className='display-3'>birth year:</span> {hero.birth_year}</p>
+                    <p><span className='display-3'>height:</span> {hero.height}</p>
+                    <p><span className='display-3'>mass:</span> {hero.mass}</p>
+                    <p><span className='display-3'>hair color:</span> {hero.hair_color}</p>
+                    <p><span className='display-3'>skin color:</span> {hero.skin_color}</p>
+                    <p><span className='display-3'>eye color:</span> {hero.eye_color}</p>
+                </div>
+            }
+        </>
+    );
+
 }
 
 export default AboutMe;
